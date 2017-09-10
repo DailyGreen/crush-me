cab0fa6a5927cf2ba5846a303107f69a805d15ad
Huduck <hhh57463@naver.com>
2017-09-10 12:50:56 +0900

    Exit PopUp Animation Update

---
+++ Assets/Resources/1_MenuScene/1_Scripts/ExitPopUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitPopUp : MonoBehaviour {
+
+    Animator ExitAni;
+
+	// Use this for initialization
+	void Start () {
+        ExitAni = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void ExitAniEnd()
+    {
+        ExitAni.enabled = false;
+    }
+}
--- Assets/Resources/1_MenuScene/1_Scripts/MenuBtnMng.cs
+++ Assets/Resources/1_MenuScene/1_Scripts/MenuBtnMng.cs
@@ -9,9 +9,11 @@ public class MenuBtnMng : MonoBehaviour
     public GameObject BackGroundGams;
     public GameObject[] SoundOnOffGams;
     public Transform SettingPopUpTr;
+    public Animator ExitAni;
     public Image[] JoystickPosImg;
     bool bPopUpAccess = false;
     bool bExit = false;
+    public bool bExitPos = false;
     void Update()
     {
         if (!SSoundMng.I.bJoyPos)
@@ -32,12 +34,27 @@ public class MenuBtnMng : MonoBehaviour
                 BackGroundGams.SetActive(false);
             }
         }
+
         if (!bExit && !bPopUpAccess)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
+            {
                 ExitGams.SetActive(true);
+                ExitAni.enabled = true;
+                bExit = true;
+            }
         }
 
+        if (bExitPos)
+        {
+            ExitGams.transform.Translate(Vector2.left * 2000f * Time.deltaTime);
+            if (ExitGams.transform.localPosition.x <= -800f)
+            {
+                ExitGams.SetActive(false);
+                bExitPos = false;
+                bExit = false;
+            }
+        }
     }
 
     public void BackGroundBtn()
@@ -51,7 +68,7 @@ public class MenuBtnMng : MonoBehaviour
 
     public void SettingBtn()
     {
-        if (!bPopUpAccess)
+        if (!bPopUpAccess && !bExit)
         {
             SettingPopUpTr.localPosition = Vector2.zero;
             BackGroundGams.SetActive(true);
@@ -107,12 +124,11 @@ public class MenuBtnMng : MonoBehaviour
 
     public void ExitNo()
     {
-        ExitGams.SetActive(false);
-        bExit = false;
+        bExitPos = true;
     }
     IEnumerator PopUpControl()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.1f);
         bPopUpAccess = false;
     }
 }
